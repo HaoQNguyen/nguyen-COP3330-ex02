f11bd7119f072b476aa6a51043b55c0de4018361
HaoQNguyen <hao.q.nguyen.2000@gmail.com>
2021-09-03 20:48:08 -0400

    initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.print("What is the input string? ");
+        Scanner input = new Scanner(System.in);
+        String s = input.next();
+        int l = s.length();
+        System.out.printf("%s has %d characters.", s, l);
+    }
+}
